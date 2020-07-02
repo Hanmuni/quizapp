96a41fe4816b942b1a9d20f4981f56c4e730af0f
Hanmuni <hchu7321@gmail.com>
2020-07-02 18:56:31 +0200

    Created Wrong/Right Answers

--- questions.js
+++ questions.js
@@ -1,3 +1,15 @@
 function nextQuestion() {
-    document.getElementById('question').innerHTML = 'Testfrage ';
+    document.getElementById('question').innerHTML = 'Wie wird eine "männliche" Primaballerina genannt?';
+    document.getElementById('answer1').innerHTML = 'Primoballerinello';
+    document.getElementById('answer2').innerHTML = 'Primoballerano';
+    document.getElementById('answer3').innerHTML = 'Primoballerini';
+    document.getElementById('answer4').innerHTML = 'Primoballerino';
+}
+
+function answer(a) {
+    if(a == 'Primoballerino') {
+        alert('Es stimmt ☺');
+
+    } else {
+        alert('Leider falsch!');}
 }
