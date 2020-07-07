87d757bfc2f52066b06af7a5e8a952f721db25da
Hanmuni <hchu7321@gmail.com>
2020-07-07 15:30:06 +0200

    Created Alerts for right/ wrong  answers

--- questions.js
+++ questions.js
@@ -1,15 +1,43 @@
+
+let answer_1 = 'Frankfurter';
+let answer_2 = 'Bremer';
+let answer_3 = 'Berliner';
+let answer_4 = 'Hamburger';
+
 function nextQuestion() {
-    document.getElementById('question').innerHTML = 'Wie wird eine "männliche" Primaballerina genannt?';
-    document.getElementById('answer1').innerHTML = 'Primoballerinello';
-    document.getElementById('answer2').innerHTML = 'Primoballerano';
-    document.getElementById('answer3').innerHTML = 'Primoballerini';
-    document.getElementById('answer4').innerHTML = 'Primoballerino';
+    document.getElementById('question').innerHTML = 'Wie nennt man ein mit gebratenem Hackfleich belegtes Brötchen?';
+    document.getElementById('answer1').innerHTML = answer_1;
+    document.getElementById('answer2').innerHTML = answer_2;
+    document.getElementById('answer3').innerHTML = answer_3;
+    document.getElementById('answer4').innerHTML = answer_4;
 }
 
 function answer(a) {
-    if(a == 'Primoballerino') {
-        alert('Es stimmt ☺');
 
-    } else {
-        alert('Leider falsch!');}
+    if(a == 1) {
+        selected_answer = answer_1; 
+    }
+
+    else if(a == 2) {
+        selected_answer = answer_2
+
+    }
+    else if(a == 3) {
+        selected_answer = answer_3
+
+    }
+
+    else {
+        selected_answer = answer_4;
+    }
+    
+    if (selected_answer == answer_4) {
+        document.getElementById('not-correct').classList.add('d-none');
+        document.getElementById('correct').classList.remove('d-none');
+    }
+
+    else {
+        document.getElementById('correct').classList.add('d-none');
+        document.getElementById('not-correct').classList.remove('d-none');
+    }
 }
