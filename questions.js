0b552c54c5c894639f8b95c54be538205f79bb44
Hanmuni <hchu7321@gmail.com>
2020-07-14 15:23:04 +0200

    created second quiz question

--- questions.js
+++ questions.js
@@ -1,52 +1,73 @@
 let question_1 = 'Wie nennt man ein mit gebratenem Hackfleich belegtes Brötchen?';
-let question_2 = 'Zweite Frage';
-let answer_1 = 'Frankfurter';
-let answer_2 = 'Bremer';
-let answer_3 = 'Berliner';
-let answer_4 = 'Hamburger';
-let new_question = 0; 
+let question_1_answer_1 = 'Frankfurter';
+let question_1_answer_2 = 'Bremer';
+let question_1_answer_3 = 'Berliner';
+let question_1_answer_4 = 'Hamburger';
+
+let question_2 = 'Was ist ein Pharisäer?';
+let question_2_answer_1 = 'Kaffee mit Rum';
+let question_2_answer_2 = 'Kakao mit Rum';
+let question_2_answer_3 = 'Tee mit Rum';
+let question_2_answer_4 = 'Fruchtsaft mit Rum';
+
+
+let right_answer = 0;
+let new_question = 0;
 let progress = 0;
 
+
 function nextQuestion() {
+    hideElements();
+
     new_question = new_question + 1;
     progress = new_question * 10;
     document.getElementById('progress-bar').innerHTML = progress + '%';
     document.getElementById('progress-bar').style.width = progress + '%';
-    if (document.getElementById('question').innerHTML == question_1) {
-        document.getElementById('question').innerHTML = question_2;
-    }
 
-    else {
-        document.getElementById('question').innerHTML = question_1;
-        document.getElementById('answer1').innerHTML = answer_1;
-        document.getElementById('answer2').innerHTML = answer_2;
-        document.getElementById('answer3').innerHTML = answer_3;
-        document.getElementById('answer4').innerHTML = answer_4;
+    if (new_question == 1) {
+        loadQuestion1();
 
     }
+
+    if (new_question == 2) {
+        loadQuestion2();
+        
+}
 
 }
 
+function loadQuestion1() {
+
+    document.getElementById('question').innerHTML = question_1;
+
+    document.getElementById('answer1').innerHTML = question_1_answer_1;
+    document.getElementById('answer2').innerHTML = question_1_answer_2;
+    document.getElementById('answer3').innerHTML = question_1_answer_3;
+    document.getElementById('answer4').innerHTML = question_1_answer_4;
+    right_answer = 4;
+}
+
+function loadQuestion2() {
+    document.getElementById('question').innerHTML = question_2;
+
+    document.getElementById('answer1').innerHTML = question_2_answer_1;
+    document.getElementById('answer2').innerHTML = question_2_answer_2;
+    document.getElementById('answer3').innerHTML = question_2_answer_3;
+    document.getElementById('answer4').innerHTML = question_2_answer_4;
+    right_answer = 1;
+
+}
+
+function hideElements() {
+    document.getElementById('new-question').classList.add('d-none');
+    document.getElementById('correct').classList.add('d-none');
+
+}
+
+
 function answer(a) {
-
-    if (a == 1) {
-        selected_answer = answer_1;
-    }
-
-    else if (a == 2) {
-        selected_answer = answer_2
-
-    }
-    else if (a == 3) {
-        selected_answer = answer_3
-
-    }
-
-    else {
-        selected_answer = answer_4;
-    }
-
-    if (selected_answer == answer_4) { // Right answer
+    
+    if (a == right_answer) { // Right answer
         document.getElementById('not-correct').classList.add('d-none');
         document.getElementById('correct').classList.remove('d-none');
         // Show next button
@@ -56,5 +77,5 @@ function answer(a) {
     else {
         document.getElementById('correct').classList.add('d-none');
         document.getElementById('not-correct').classList.remove('d-none');
-    }
+    }   
 }
